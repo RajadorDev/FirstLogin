110a6cb51c78339630e32fb188584ebc7dbfb3e5
RajadorDev <rajadordeveloper@gmail.com>
2025-01-26 19:41:57 -0300

    Terminado

---
+++ src/FirstLogin/Plugin.php
@@ -0,0 +1,34 @@
+<?php
+
+declare (strict_types=1);
+
+namespace FirstLogin;
+
+use pocketmine\event\EventPriority;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+final class Plugin extends PluginBase
+{
+
+    protected Config $joinnedListFile;
+
+    protected function onEnable(): void
+    {
+        $this->joinnedListFile = new Config($this->getDataFolder() . 'joinned.txt', Config::ENUM);
+        $this->getServer()->getPluginManager()->registerEvent(
+            PlayerJoinEvent::class,
+            function (PlayerJoinEvent $event) : void {
+                $player = $event->getPlayer();
+                $username = strtolower($player->getName());
+                if (!$this->joinnedListFile->exists($username))
+                {
+                    $this->getServer()->broadcastMessage("§8-  §7è a primeira vez do jogador §f{$player->getName()} jogando neste servidor! Seja bem vindo");
+                    $this->joinnedListFile->set($username);
+                    $this->joinnedListFile->save();
+                }
+            }, EventPriority::MONITOR, $this
+        );
+    }
+}
